832290ad98c1f95763f39ff81db3171f997658ea
suryaa6666 <suryaelidanto@gmail.com>
2023-06-06 13:40:03 +0700

    feat(api.php): add TodoController class and CRUD endpoints for todos feat(todos.json): add initial todo data for testing purposes

--- routes/api.php
+++ routes/api.php
@@ -14,12 +14,125 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/todos', [TodoController::class, 'findTodos']);
+Route::get('/todos/{id}', [TodoController::class, 'getTodo']);
+Route::post('/todos', [TodoController::class, 'createTodo']);
+Route::patch('/todos/{id}', [TodoController::class, 'updateTodo']);
+Route::delete('/todos/{id}', [TodoController::class, 'deleteTodo']);
 
-Route::get('/hello', function () {
-    return response()->json([
-        'message' => 'Hello World! 😎'
-    ]);
-});
+class TodoController
+{
+    private $todosFile = __DIR__ . '/todos.json';
+
+    public function findTodos()
+    {
+        $todos = json_decode(file_get_contents($this->todosFile));
+
+        return response()->json([
+            'message' => $todos
+        ]);
+    }
+
+    public function getTodo($id)
+    {
+        $todo = null;
+        $isGetTodo = false;
+
+        $todos = json_decode(file_get_contents($this->todosFile));
+
+        foreach ($todos as $item) {
+            if ($id === $item->id) {
+                $isGetTodo = true;
+                $todo = $item;
+                break;
+            }
+        }
+
+        if (!$isGetTodo) {
+            return response()->json([
+                "code" => 404,
+                "message" => "ID : $id not found"
+            ], 404);
+        }
+
+        return response()->json($todo);
+    }
+
+    public function createTodo(Request $request)
+    {
+        $todos = json_decode(file_get_contents($this->todosFile));
+
+        $todo = [
+            'id' => $request->id,
+            'title' => $request->title,
+            'isDone' => false
+        ];
+
+        array_push($todos, $todo);
+
+        file_put_contents($this->todosFile, json_encode($todos));
+
+        return response()->json([
+            "code" => 201,
+            'message' => 'Todo created successfully'
+        ]);
+    }
+
+    public function updateTodo(Request $request, $id)
+    {
+        $data = $request->json()->all();
+        $isGetTodo = false;
+
+        $todos = json_decode(file_get_contents($this->todosFile));
+
+        foreach ($todos as $index => $item) {
+            if ($id === $item->id) {
+                $isGetTodo = true;
+                $todos[$index] = $data;
+                break;
+            }
+        }
+
+        if (!$isGetTodo) {
+            return response()->json([
+                "code" => 404,
+                "message" => "ID : $id not found"
+            ], 404);
+        }
+
+        file_put_contents($this->todosFile, json_encode($todos));
+
+        return response()->json($data);
+    }
+
+    public function deleteTodo($id)
+    {
+        $isGetTodo = false;
+        $index = 0;
+
+        $todos = json_decode(file_get_contents($this->todosFile));
+
+        foreach ($todos as $idx => $item) {
+            if ($id === $item->id) {
+                $isGetTodo = true;
+                $index = $idx;
+                break;
+            }
+        }
+
+        if (!$isGetTodo) {
+            return response()->json([
+                "code" => 404,
+                "ID : $id not found"
+            ], 404);
+        }
+
+        array_splice($todos, $index, 1);
+        file_put_contents($this->todosFile, json_encode($todos));
+
+        return response()->json([
+            "code" => 204,
+            'message' => 'Todo deleted successfully'
+        ]);
+    }
+}
